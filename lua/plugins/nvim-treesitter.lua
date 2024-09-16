83e4275f2e8deea67576d5ff3f002c6453d66640
ram <ram15.panjwani@gmail.com>
2024-09-16 19:38:23 +0530

    add more programming languages

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,12 +5,11 @@ return {
 		config = function()
 			require 'nvim-treesitter.configs'.setup {
 				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "javascript", "html", "java" },
-				sync_install = false,
 				auto_install = true,
 				highlight = {
 					enable = true,
-					additional_vim_regex_highlighting = false,
 				},
+				indent = {enable = true},
 			}
 
 			vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>', {});
